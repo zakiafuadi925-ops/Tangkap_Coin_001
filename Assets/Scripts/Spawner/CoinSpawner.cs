cf299a1ccd5b6ea72923e75fc7e33be45ac96590
zakiafuadi925-ops <zakiafuadi925@gmail.com>
2026-07-18 19:43:58 +0700

    Update CoinSpawner

--- Assets/Scripts/Spawner/CoinSpawner.cs
+++ Assets/Scripts/Spawner/CoinSpawner.cs
@@ -21,4 +21,18 @@ public class CoinSpawner : MonoBehaviour
     {
         Instantiate(coinPrefab, transform.position, Quaternion.identity);
     }
+
+    void Update()
+    {
+        int score = ScoreManager.Instance.CurrentScore;
+
+        if (score >= 100)
+            spawnInterval = 0.7f;
+        else if (score >= 50)
+            spawnInterval = 0.9f;
+        else if (score >= 20)
+            spawnInterval = 1.2f;
+        else
+            spawnInterval = 1.5f;
+    }
 }
