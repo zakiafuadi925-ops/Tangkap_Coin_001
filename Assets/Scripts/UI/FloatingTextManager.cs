f4e37b9652ec999dabfed46293e5f9aead8910d9
zakiafuadi925-ops <zakiafuadi925@gmail.com>
2026-07-18 18:49:06 +0700

    UPDATE Floating Text Manager

---
+++ Assets/Scripts/UI/FloatingTextManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FloatingTextManager : MonoBehaviour
+{
+    public static FloatingTextManager Instance;
+
+    [SerializeField] private GameObject floatingTextPrefab;
+    [SerializeField] private Canvas canvas;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ShowText(string text, Vector3 worldPosition)
+    {
+        GameObject obj = Instantiate(floatingTextPrefab, canvas.transform);
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
+
+        obj.GetComponent<RectTransform>().position = screenPosition;
+
+        obj.GetComponent<FloatingText>().SetText(text);
+    }
+}
