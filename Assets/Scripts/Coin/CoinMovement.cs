7a80db56cbae06a56dc96f038b92cf496d7413fc
zakiafuadi925-ops <zakiafuadi925@gmail.com>
2026-07-18 11:46:54 +0700

    membuat perbedaan kecepatan jatuh coin

--- Assets/Scripts/Coin/CoinMovement.cs
+++ Assets/Scripts/Coin/CoinMovement.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 
 public class CoinMovement : MonoBehaviour
 {
-    public float idleSpeed = 1.5f;
-    public float fallSpeed = 5f;
-    public float idleDuration = 1f;
+    public float minIdleSpeed = 1f;
+    public float maxIdleSpeed = 2f;
+    public float minFallSpeed = 4f;
+    public float maxFallSpeed = 7f;
+    public float minIdleTime = 0.5f;
+    public float maxIdleTime = 1.5f;
 
+
+    private float idleSpeed;
+    private float fallSpeed;
     private Animator animator;
     private bool isFalling = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        Invoke(nameof(StartFall), idleDuration);
+        idleSpeed = Random.Range(minIdleSpeed, maxIdleSpeed);
+        fallSpeed = Random.Range(minFallSpeed, maxFallSpeed);
+
+        float idleTime = Random.Range(minIdleTime, maxIdleTime);
+        Invoke(nameof(StartFall), idleTime);
     }
 
     void Update()
